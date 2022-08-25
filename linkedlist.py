7c0cb63b147884dd87f8e21a7992529c44533143
onofrelima <onofre.felix@domrock.ai>
2022-08-25 20:33:47 -0300

    Praticing linked list

--- linkedlist.py
+++ linkedlist.py
@@ -25,6 +25,51 @@ class LinkedList(object):
     def __init__(self):
         self.head = None
 
+    def is_empty(self):
+        return self.head is None
+
+    def add_item(self, item):
+        current = Node(item)
+        current.set_next(self.head)
+        self.head = current
+
+    def size(self) -> int:
+        current = self.head
+        count = 0
+        while current is not None:
+            count +=1
+            current = current.get_next()
+
+        return count
+
+    def is_value_exist(self, value):
+        current = self.head
+        is_exists = False
+        while current is not None:
+            if current.get_value() == value:
+                is_exists = True
+            current = current.get_next()
+        return is_exists
+
+    def remove_item(self, value: T) -> bool:
+        current = self.head
+        previous: Node = None
+        found = False
+
+        while current is not None:
+            if current.get_value() == value:
+                found = True
+                break
+            else:
+                previous = current
+                current = current.get_next()
+
+        if previous is None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+
+        return found
 
 
 
--- test_linkedlist.py
+++ test_linkedlist.py
@@ -1,4 +1,4 @@
-from linkedlist import LinkedList
+from linkedlist import LinkedList,Node
 
 
 class TestClass:
@@ -8,4 +8,34 @@ class TestClass:
         assert isinstance(self.link_list, LinkedList)
 
     def test_if_head_is_null(self):
-        assert link_list.get_head() is None
+        assert self.link_list.is_empty() is True
+
+    def test_add_item(self):
+        self.link_list.add_item(10)
+        self.link_list.add_item(20)
+        assert self.link_list.is_empty() is False
+
+    def test_size(self):
+        self.link_list.add_item(10)
+        self.link_list.add_item(20)
+        val_expected = 2
+        assert self.link_list.size() == val_expected
+
+    def test_if_item_exist(self):
+        value = 20
+        self.link_list.add_item(10)
+        self.link_list.add_item(20)
+        val_expected = True
+        assert self.link_list.is_value_exist(value) == val_expected
+
+    def test_remove_item(self):
+        value = 30
+        self.link_list.add_item(10)
+        self.link_list.add_item(20)
+        self.link_list.add_item(30)
+        val_expected = True
+        assert self.link_list.remove_item(value) == val_expected
+
+
+
+
